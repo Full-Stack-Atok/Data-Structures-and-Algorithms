eb689ee5131e9ec6cce18558a893744492f29839
Full-Stack-Atok <komsaykerby@gmail.com>
2025-01-16 18:45:14 +0800

    Day 1

---
+++ Task 3.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input("Enter first integer: "))
+    b = int(input("Enter second integer: "))
+    
+    print(f"Sum of {a} and {b} is: {a + b}") 
+    print(f"Difference of {a} and {b} is: {a - b}")
+    print(f"Product of {a} and {b} is: {a * b}")
+    
